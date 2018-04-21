0a330b15c5e5c70c8ba1f0343f9ce08144be82e6
cyb3rDem0n <dagostinogiuseppe@outlook.com>
2018-04-21 11:23:59 +0200

    chenge baseball method string output

--- src/com/course/springdemo/BaseballCoach.java
+++ src/com/course/springdemo/BaseballCoach.java
@@ -5,7 +5,7 @@ public class BaseballCoach implements Coach {
 	@Override
 	public String getDailyWorkout() {
 		// TODO Auto-generated method stub
-		return "Allenati porco";
+		return "Training Baseball";
 	}
 
 }
